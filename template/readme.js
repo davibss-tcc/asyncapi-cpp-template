b799d9142d60929cfa68786ccc52757ff9644896
davibss <davi.sousa@ccc.ufcg.edu.br>
2023-09-26 21:20:55 -0300

    feat(readme): add instructions for hostname and update code

--- template/readme.js
+++ template/readme.js
@@ -26,6 +26,22 @@ In this root folder:
 1. Click on **Configure All Projects**
 3. Choose the \`g++\` option
 2. Click in the bottom button in VSCode: **Launch the selected target in the terminal window [simulated_server]**
+
+## Changing the hostname
+In the \`src/simulated_server.cpp\` file you can change this line to be the host you need.
+\`\`\`cpp
+#define mqtt_host "127.0.0.1"
+\`\`\`
+After you change this line you'll have to compile again your code.
+
+## Updating the generated code
+If your specification has changed you can generate again the main code using this process:
+\`\`\`sh
+ag {spec_file_path} https://github.com/davibss-tcc/asyncapi-cpp-template -o ./ --force-write -p {your_params}
+\`\`\`
+**CAUTION!!** This operation will change almost all files, except by \`src/services/communication-layer-impl.cpp\` and \
+\`src/services/topics-impl.cpp\`. If the overwritten changes break this files you'll have to fix by yourself. To overwrite \
+this special files you would have to delete them and then generate again.
 `}
     </Text>
 </File>
